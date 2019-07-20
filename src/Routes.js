90c0885c411f058bedcf589251418c8ecf8ed259
mshayanabbasi <mshayanabbasi>
2019-07-20 20:34:06 +0500

    quiz

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Login from './components/Login/Login';
+
+class Router extends Component {
+    
+    render() { 
+        return ( 
+            <BrowserRouter>
+                <Route path="/" component={Login}/>
+            </BrowserRouter>
+         );
+    }
+}
+ 
+export default Router;
