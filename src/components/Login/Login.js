237e12da7ec7f09031fa0eb2a33c66cd56497c81
mshayanabbasi <mshayanabbasi>
2019-07-21 12:04:14 +0500

    components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,15 +13,40 @@ class Login extends Component {
           currentUser: null,
           errors: {
               hasError: false,
-              errorObj: {}
+              errorObj: {},
+              serverError: ""
           },
           name: "",
           pass: ""
      }
+     validate = () => {
+         const { name, pass, errors } = this.state
+         if(name.length > 3) {
+             errors.hasError = true
+             errors.errorObj["name"] ? 
+             errors.errorObj["name"] = {message: "Name Can't Be Less Then 4 Characters"} :
+             errors.errorObj["name"].message += "Name Can't Be Less Then 4 Characters"
+         }
+         return errors.hasError ? errors : {
+             hasError: false,
+             errorObj: {},
+             serverError: ""
+         }
+     }
      onSubmit = (event) => {
         event.preventDefault()
         const { users, errors, name, pass } = this.state
-     }
+        var currentUser = users.filter((user) => {
+            return user.name === name && user.pass === pass
+        })    
+        if(currentUser.length) {
+            localStorage.setItem("currentUser", JSON.stringify(currentUser[0]))
+        }
+        else {
+            errors.serverError = "Wrong Credentials"
+            this.setState({ errors })
+        }
+    }
     render() { 
         const { name, errors, pass } = this.state
         return ( 
@@ -29,6 +54,7 @@ class Login extends Component {
                 <div className="login-form-wrapper">
                 <form onSubmit={(event) => this.onSubmit(event)}>
                     <h1>Login</h1>
+                    {errors.serverError && <p><strong className="error">{errors.serverError}</strong></p>}
                     <Input 
                         type="text"
                         value={name}
